0a2c0c533c1a9e0e333e65d002b575275dec2292
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-07 08:47:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,46 @@
 import telebot
+import os
+from PIL import Image
 
 # Replace with your bot token
 BOT_TOKEN = '2118571380:AAGR-_rB53MsMon35q5i2B3Nw7RJqPXHy18'
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-@bot.message_handler(commands=['start'])
-def send_welcome_message(message):
-    # Create an inline keyboard markup
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    hi_button = telebot.types.InlineKeyboardButton(text="Hi", callback_data="hi_button")
-    keyboard.add(hi_button)
+@bot.message_handler(func=lambda message: message.content_type == 'photo')
+def handle_image(message):
+    # Get the file ID of the image
+    file_id = message.photo[-1].file_id
 
-    # Send the welcome message with inline keyboard
-    bot.send_message(message.chat.id, 'Hi! Click the button below to greet me.', reply_markup=keyboard)
+    # Download the image file
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
 
-@bot.callback_query_handler(func=lambda call: call.data == "hi_button")
-def handle_hi_button_click(call):
-    bot.send_message(call.message.chat.id, "Hi there! How can I help you today?")
+    # Save the downloaded image temporarily
+    temp_image_path = f'temp_{file_id}.jpg'
+    with open(temp_image_path, 'wb') as f:
+        f.write(downloaded_file)
+
+    # Upscale the image using Pillow
+    upscaled_image_path = f'upscaled_{file_id}.jpg'
+    upscale_image(temp_image_path, upscaled_image_path)
+
+    # Send the upscaled image to the chat
+    with open(upscaled_image_path, 'rb') as f:
+        bot.send_photo(message.chat.id, f, caption="Upscaled Image")
+
+    # Delete temporary files
+    os.remove(temp_image_path)
+    os.remove(upscaled_image_path)
+
+def upscale_image(input_path, output_path):
+    # Open the image
+    original_image = Image.open(input_path)
+
+    # Upscale the image by a factor of 2 using nearest neighbor interpolation
+    upscaled_image = original_image.resize((original_image.width * 2, original_image.height * 2), Image.NEAREST)
+
+    # Save the upscaled image
+    upscaled_image.save(output_path)
 
 bot.polling()
