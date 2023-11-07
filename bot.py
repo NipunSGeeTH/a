9845f4afba4b0da0ce30d7ae2e39b1e913cc39a6
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-07 07:48:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,27 @@
 import telebot
-import requests
+import urllib.request
 
 # Replace with your bot token
 BOT_TOKEN = '2118571380:AAGR-_rB53MsMon35q5i2B3Nw7RJqPXHy18'
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+def download_media(media_link, media_type, bot, chat_id):
+    # Check if the media exists
+    try:
+        with urllib.request.urlopen(media_link) as response:
+            if response.status == 200:
+                # Send the media to the chat
+                if media_type == 'image':
+                    bot.send_photo(chat_id, response.read())
+                elif media_type == 'video':
+                    bot.send_video(chat_id, response.read())
+            else:
+                # Notify the user that the media could not be downloaded
+                bot.send_message(chat_id, f'Failed to download the {media_type}.')
+    except urllib.error.URLError:
+        # Handle network errors
+        bot.send_message(chat_id, f'Failed to download the {media_type}.')
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
@@ -23,20 +40,15 @@ def handle_message(message):
         # Send the appropriate media if the type is recognized
         if media_type:
             # Download the media from the link
-            response = requests.get(media_link, stream=True)
-
-            # Check if the media exists
-            if response.status_code == 200:
-                # Send the media to the chat
-                if media_type == 'image':
-                    bot.send_photo(message.chat.id, response.raw.read())
-                elif media_type == 'video':
-                    bot.send_video(message.chat.id, response.raw.read())
-            else:
-                # Notify the user that the media could not be downloaded
-                bot.send_message(message.chat.id, f'Failed to download the {media_type}.')
+            download_media(media_link, media_type, bot, message.chat.id)
         else:
             # If it's not a recognized media link, handle it as a regular message
             bot.send_message(message.chat.id, 'Please send an image or video link.')
+
+    # Additional functionality
+    if message.text == '/start':
+        bot.send_message(message.chat.id, 'Hello! I am a bot that can download images and videos.')
+    elif message.text == '/help':
+        bot.send_message(message.chat.id, 'To download an image or video, simply send me the link.')
 
 bot.polling()
